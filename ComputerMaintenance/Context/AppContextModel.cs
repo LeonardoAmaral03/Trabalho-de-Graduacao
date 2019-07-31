443895f265d57faf99de6a02d42041957cce0a37
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2019-07-31 19:38:12 -0300

    Configurando contexto e connectionString

---
+++ ComputerMaintenance/Context/AppContextModel.cs
@@ -0,0 +1,22 @@
+﻿using ComputerMaintenance.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputerMaintenance.Context
+{
+    public class AppContextModel : DbContext
+    {
+        public DbSet<Computer> Computers { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Maintenance> Maintenances { get; set; }
+        public DbSet<ItemComputer> ItemComputers { get; set; }
+        public DbSet<MaintenanceItem> MaintenanceItems { get; set; }
+
+        public AppContextModel(DbContextOptions<AppContextModel> options) : base(options)
+        {
+        }
+    }
+}
