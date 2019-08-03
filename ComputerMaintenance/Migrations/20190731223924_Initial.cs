12ae54fbcc7d306652dd7c1db64b282efb9eaaaa
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2019-08-03 18:12:51 -0300

    Criação da primeira migration

---
+++ ComputerMaintenance/Migrations/20190731223924_Initial.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ComputerMaintenance.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Computers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Computers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Brand = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Maintenances",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    ActiActivity = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maintenances", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemComputers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    AcquisitionDate = table.Column<DateTime>(nullable: false),
+                    ComputerId = table.Column<Guid>(nullable: true),
+                    ItemId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemComputers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemComputers_Computers_ComputerId",
+                        column: x => x.ComputerId,
+                        principalTable: "Computers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ItemComputers_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MaintenanceItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Period = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    ItemId = table.Column<Guid>(nullable: true),
+                    MaintenanceId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaintenanceItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MaintenanceItems_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MaintenanceItems_Maintenances_MaintenanceId",
+                        column: x => x.MaintenanceId,
+                        principalTable: "Maintenances",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemComputers_ComputerId",
+                table: "ItemComputers",
+                column: "ComputerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemComputers_ItemId",
+                table: "ItemComputers",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaintenanceItems_ItemId",
+                table: "MaintenanceItems",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaintenanceItems_MaintenanceId",
+                table: "MaintenanceItems",
+                column: "MaintenanceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemComputers");
+
+            migrationBuilder.DropTable(
+                name: "MaintenanceItems");
+
+            migrationBuilder.DropTable(
+                name: "Computers");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Maintenances");
+        }
+    }
+}
