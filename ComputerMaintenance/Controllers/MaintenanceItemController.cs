86e5cd3c00ada652ac9f9dc46f67544bbf743392
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2019-10-12 21:44:26 -0300

    Implementada lógica que agenda novas datas de manutenções ao inserir nova manutenção em item.

--- ComputerMaintenance/Controllers/MaintenanceItemController.cs
+++ ComputerMaintenance/Controllers/MaintenanceItemController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using ComputerMaintenance.Context;
 using ComputerMaintenance.Models;
 using ComputerMaintenance.Models.ViewModels;
+using static ComputerMaintenance.Models.Enum.EnumStatus;
 
 namespace ComputerMaintenance.Controllers
 {
@@ -161,6 +162,74 @@ namespace ComputerMaintenance.Controllers
         public async Task<ActionResult<MaintenanceItem>> PostMaintenanceItem(MaintenanceItem maintenanceItem)
         {
             _context.MaintenanceItems.Add(maintenanceItem);
+            //try
+            //{
+            //    await _context.SaveChangesAsync();
+            //}
+            //catch (DbUpdateException)
+            //{
+            //    if (MaintenanceItemExists(maintenanceItem.ItemId))
+            //    {
+            //        return Conflict();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
+
+            List<ScheduleItemComputer> scheduleItemComputerList = await (from sic in _context.ScheduleItemComputers
+                                                    where sic.ItemId == maintenanceItem.ItemId
+                                                    select new ScheduleItemComputer
+                                                    {
+                                                        Id = sic.Id,                                                        
+                                                    }).ToListAsync();
+
+            if (scheduleItemComputerList.Count() > 0)
+            {
+                ScheduleMaintenanceItem scheduleMaintenanceItem;
+                List<ScheduleMaintenanceItem> scheduleMaintenanceItems = new List<ScheduleMaintenanceItem>();
+                Schedule schedule;
+                List<Schedule> schedules = new List<Schedule>();
+
+                var registrationDate = DateTime.Now;
+                var dateYearsPeriod = registrationDate.AddYears(4).Year;
+
+                foreach (var scheduleItemComputer in scheduleItemComputerList)
+                {
+                    scheduleMaintenanceItem = new ScheduleMaintenanceItem();
+
+                    scheduleMaintenanceItem.Id = Guid.NewGuid();
+                    scheduleMaintenanceItem.ScheduleItemComputerId = scheduleItemComputer.Id;
+                    scheduleMaintenanceItem.MaintenanceId = maintenanceItem.MaintenanceId;
+
+                    scheduleMaintenanceItems.Add(scheduleMaintenanceItem);
+                }
+
+                foreach (var scheduleMI in scheduleMaintenanceItems)
+                {
+                    var dateAddMonth = registrationDate;
+
+                    while (dateAddMonth.Year < dateYearsPeriod)
+                    {
+                        schedule = new Schedule();
+
+                        schedule.Id = Guid.NewGuid();
+                        schedule.ScheduleMaintenanceItemId = scheduleMI.Id;
+                        schedule.Status = Status.Pending;
+
+                        dateAddMonth = dateAddMonth.AddMonths(maintenanceItem.Period);
+
+                        schedule.MaintenanceDate = dateAddMonth;
+
+                        schedules.Add(schedule);
+                    }
+                }
+
+                _context.ScheduleMaintenanceItems.AddRange(scheduleMaintenanceItems);
+                _context.Schedules.AddRange(schedules);
+            }
+
             try
             {
                 await _context.SaveChangesAsync();
